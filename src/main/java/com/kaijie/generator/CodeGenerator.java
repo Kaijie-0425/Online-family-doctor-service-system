c1a8589a4e27bf40e294f4775fa570d20fbd1117
Kaijie-0425 <leikaijie0425@outlook.com>
2026-03-13 02:01:33 +0800

    lkj

--- src/main/java/com/kaijie/generator/CodeGenerator.java
+++ src/main/java/com/kaijie/generator/CodeGenerator.java
@@ -1,9 +1,7 @@
 package com.kaijie.generator;
 
 public class CodeGenerator {
-    //打印Hello World
-    public static void main(String[] args) {
-         System.out.println("Hello World");  }
+
 }
 
 
